b1a2450f1b736978f881769862f687a850ee39e7
Sean Thorpe <sean.thorpe@west-mec.org>
2015-09-17 15:21:12 -0700

    put in my answers

--- basics.js
+++ basics.js
@@ -1,37 +1,46 @@
-// What is the return value of the below code sample? Provide a sentence or two of explanation.
 typeof( 15 );
+   //number, This is the answer because when you use a numbers with commas its considered a string.
 
-// What is the return value of the below code sample? Provide a sentence or two of explanation.
+
+
 typeof( "hello" );
+  //string, this is a string because when it has comma, text, comma in the parentheses.
 
-// What is the return value of the below code sample? Provide a sentence or two of explanation.
+
 typeof( [ "dog", "cat", "horse" ] );
+ //object, this is an object because anything in parentheses are considered objects.
 
-// What is the return value of the below code sample? Provide a sentence or two of explanation.
+
 typeof( NaN );
+ //number, this is considered a number because even though it is used with letters it is meant for when a number doesn't exist.
 
-// What is the return value of the below code sample? Provide a sentence or two of explanation.
+
 "hamburger" + "s";
+  //hamburgers, hamburger and s are can be added since there is not command to add them.
 
-// What is the return value of the below code sample? Provide a sentence or two of explanation.
+
 "hamburgers" - "s";
+   //NaN, hamburger and s are both strings... cant be subtracted.
 
-// What is the return value of the below code sample? Provide a sentence or two of explanation.
+
 "johnny" + 5;
+  //NaN, this is not a number since you can't add a string and a number.
 
-// What is the return value of the below code sample? Provide a sentence or two of explanation.
+
 99 * "luftbaloons";
 
-// What will the contents of the below array be after the below code sample is executed.
+
 var numbers = [ 2, 4, 6, 8 ];
 numbers.pop();
 numbers.push( 10 );
 numbers.unshift( 3 );
+   //[ 3, 2, 4, 6, 10 ] this is the answer since pop gets rid of the last one (8), push add 10 to the end, and unshift puts 3 in the front.
 
 // What is the return value of the below code sample?
 var morse = [ "dot", "pause", "dot" ];
 var moreMorse = morse.join( " dash " );
 moreMorse.split( " " );
+
 
 // What will the contents of the below array be after the below code sample is executed.
 var bands = [];
